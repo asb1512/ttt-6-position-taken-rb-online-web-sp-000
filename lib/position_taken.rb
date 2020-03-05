90d50c5f99fb6a0d890b8e5a74eb02a497790099
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-05 03:16:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,7 @@
 require 'pry'
 
 def position_taken?(board, index)
+binding.pry
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   elsif board[index] == "X" || board[index] == "O"
